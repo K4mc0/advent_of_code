4a080648fcd44b41f233a2525ca128843a92e4fb
k4mc0 <bekec.kamil@gmail.com>
2022-12-20 16:45:18 +0100

    task2

--- 2022/09/task9.py
+++ 2022/09/task9.py
@@ -4,12 +4,8 @@ def is_touching(xH, xT, yH, yT):
             return True
     return False
 
-def make_move(command, xH, xT, yH, yT):
-    print("Head:", xH, yH, "Tail:", xT, yT)
-    tail_moves = {(xT, yT)}
-    direction = command.split(" ")[0]
-    moves = int(command.split(" ")[1])
-    for i in range(moves):
+def make_move(direction, xH, xT, yH, yT, head):
+    if (head):
         if (direction == 'L'):
             xH -= 1
         elif (direction == 'R'):
@@ -18,52 +14,57 @@ def make_move(command, xH, xT, yH, yT):
             yH += 1
         elif (direction == 'D'):
             yH -= 1
-        if (not is_touching(xH, xT, yH, yT)):
-            if (xH == xT):
-                if (yT == yH + 2 ):
-                    yT -= 1 
-                elif (yT == yH - 2):
-                    yT += 1
-            elif (yH == yT):
-                if (xT == xH + 2 ):
-                    xT -= 1 
-                elif (xT == xH - 2):
-                    xT += 1
-            else:
-                if (xH < xT):
-                    if (yH < yT):
-                        xT -= 1
-                        yT -= 1
-                    else:
-                        xT -= 1
-                        yT += 1
+    if (not is_touching(xH, xT, yH, yT)):
+        if (xH == xT):
+            if (yT == yH + 2 ):
+                yT -= 1 
+            elif (yT == yH - 2):
+                yT += 1
+        elif (yH == yT):
+            if (xT == xH + 2 ):
+                xT -= 1 
+            elif (xT == xH - 2):
+                xT += 1
+        else:
+            if (xH < xT):
+                if (yH < yT):
+                    xT -= 1
+                    yT -= 1
                 else:
-                    if (yH < yT):
-                        xT += 1
-                        yT -= 1
-                    else:
-                        xT += 1
-                        yT += 1
-        tail_moves.add((xT,yT))
-        print (i, "move", direction, "Head:", xH, yH, "Tail:", xT, yT)  
-    print (moves, "moves", direction, "Head:", xH, yH, "Tail:", xT, yT)                
-    return (xH, xT, yH, yT, tail_moves)
+                    xT -= 1
+                    yT += 1
+            else:
+                if (yH < yT):
+                    xT += 1
+                    yT -= 1
+                else:
+                    xT += 1
+                    yT += 1           
+    return (xH, xT, yH, yT)
 
-def main():
+def solution(rope_length):
     with open("input.txt", "r") as file:
         lines = file.read().splitlines()
-    xH, xT, yH, yT = 0, 0, 0, 0
-    tail_moves = {(xT, yT)}
+    x = [0] * rope_length
+    y = [0] * rope_length
+    tail_moves = {(x[rope_length-1], y[rope_length-1])}
+    head = True
     for line in lines:
-        tmp = make_move(line, xH, xT, yH, yT)
-        print(tmp)
-        xH, xT, yH, yT = tmp[0], tmp[1], tmp[2], tmp[3]
-        for t in tmp[4]:
-            tail_moves.add(t)
-    print(len(tail_moves))
-    if (is_touching(4,3,0,1)):
-        print ("ano")
-    else: print("nie")
+        direction = line.split(" ")[0]
+        moves = int(line.split(" ")[1])
+        for i in range(moves):
+            for j in range(rope_length-1):
+                if (j == 0):
+                    head = True
+                else: head = False
+                tmp = make_move(direction, x[j], x[j+1], y[j], y[j+1], head)
+                x[j], x[j+1], y[j], y[j+1] = tmp[0], tmp[1], tmp[2], tmp[3]
+            tail_moves.add((x[rope_length-1], y[rope_length-1]))
+    return len(tail_moves)
+
+def main():
+    print("Result for task 1 is", solution(2))
+    print("Result for task 2 is", solution(10))
 
 if __name__ == "__main__":
     main()
